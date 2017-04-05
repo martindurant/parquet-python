b6e005dd7e7b1d5e068fc95796208c336749aa22
beckermr <mbecker@civisanalytics.com>
2017-04-05 17:53:12 -0500

    TST added test of seeding for float quantization

--- fastparquet/quantize/tests/test_quantize.py
+++ fastparquet/quantize/tests/test_quantize.py
@@ -1,6 +1,7 @@
 import numpy as np
 from fastparquet import quantize
 
+
 def test_quantize_float32():
     seed = 2454389
     rng = np.random.RandomState(seed=seed)
@@ -10,6 +11,7 @@ def test_quantize_float32():
     std = np.std(data - check_data)
     assert std < 0.05, "RICE float quantization and compression for float 32 is broken!"
 
+
 def test_quantize_float64():
     seed = 2454389
     rng = np.random.RandomState(seed=seed)
@@ -18,3 +20,33 @@ def test_quantize_float64():
     check_data = quantize.dequantize_and_decompress(buff)
     std = np.std(data - check_data)
     assert std < 0.05, "RICE float quantization and compression for float 64 is broken!"
+
+
+def test_seeding_float32():
+    seed = 2454389
+    rng = np.random.RandomState(seed=seed)
+    data = rng.normal(size=100000).astype(np.float32)
+
+    buff = quantize.quantize_and_compress_float32(data, 0.1, 0.0, seed=1234)
+    check_data = quantize.dequantize_and_decompress(buff)
+
+    buff2 = quantize.quantize_and_compress_float32(data, 0.1, 0.0, seed=1234)
+    check_data2 = quantize.dequantize_and_decompress(buff2)
+
+    assert np.array_equal(check_data, check_data2), (
+        "RICE float quantization is not deterministic for float 32!")
+
+
+def test_seeding_float64():
+    seed = 2454389
+    rng = np.random.RandomState(seed=seed)
+    data = rng.normal(size=100000).astype(np.float64)
+
+    buff = quantize.quantize_and_compress_float64(data, 0.1, 0.0, seed=1234)
+    check_data = quantize.dequantize_and_decompress(buff)
+
+    buff2 = quantize.quantize_and_compress_float64(data, 0.1, 0.0, seed=1234)
+    check_data2 = quantize.dequantize_and_decompress(buff2)
+
+    assert np.array_equal(check_data, check_data2), (
+        "RICE float quantization is not deterministic for float 64!")
