2a979362e248d659f5b76ee8050069570e067f8c
Prokopios Gryllos <prokopis.gryllos@sentiance.com>
2018-11-05 16:33:15 +0100

    fix version number

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ install_requires.append('pytest-runner')
 
 setup(
     name='fastparquet',
-    version='0.1.6.feature.optional_columns_to_float.0',
+    version='0.1.6',
     description='Python support for Parquet file format',
     author='Martin Durant',
     author_email='mdurant@continuum.io',
