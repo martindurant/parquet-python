b077dcae64652ea01b4710d5374c38300dbab761
Martin Durant <martin.durant@alumni.utoronto.ca>
2022-11-11 13:11:49 -0500

    SCM versioning (finally)

--- fastparquet/__init__.py
+++ fastparquet/__init__.py
@@ -5,3 +5,5 @@ from .writer import write, update_file_custom_metadata
 from . import core, schema, converted_types, api
 from .api import ParquetFile
 from .util import ParquetException
+
+from ._version import __version__
